692885161c790c35c6c02fcb7b7f88814df118b9
nwainwri <nathanwainwright@gmail.com>
2019-09-06 18:26:47 -0700

    Setup project.yaml to have UITests enabled.

---
+++ xkcdTests/XkcdTests.swift
@@ -0,0 +1,36 @@
+//
+//  xkcdTests.swift
+//  xkcdTests
+//
+//  Created by Nathan Wainwright on 2019-09-06.
+//
+
+import XCTest
+
+class XkcdTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+  func testPassOrFail() {
+    XCTAssertTrue(false)
+  }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
